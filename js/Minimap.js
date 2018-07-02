dfc2fb77ef5ceb5edcf08778d0d7210db927736a
bonochof <ryos427@yahoo.co.jp>
2018-07-03 07:21:18 +0900

    add draw method

--- js/Minimap.js
+++ js/Minimap.js
@@ -1,16 +1,44 @@
-function loadMap() {
-  drawMiniMap();
-}
+class MiniMap {
+  constructor() {
+    this.cv = document.getElementById('miniMap');
+    this.ct = this.cv.getContext('2d');
+  }
 
-function drawMiniMap() {
-  var cv = document.getElementById('miniMap');
-  var ct = cv.getContext('2d');
-  
-  ct.globalAlpha = 0.8;
+  convertMapPosX(playerX) {
+    return (playerX + 60) * 2.3;
+  }
 
-  ct.fillStyle = '#ccffcc';
-  ct.fillRect(0, 0, cv.width, cv.height);
+  convertMapPosY(playerY) {
+    return playerY + 50 * 1.5;
+  }
 
-  ct.strokeStyle = '#00ff00';
-  ct.strokeRect(0, 0, cv.width, cv.height);
+  clearMiniMap() {
+    this.ct.clearRect(0, 0, this.cv.width, this.cv.height);
+  }
+
+  drawMiniMap() {
+    this.ct.globalAlpha = 0.6;
+
+    this.ct.fillStyle = '#ccffcc';
+    this.ct.fillRect(0, 0, this.cv.width, this.cv.height);
+
+    this.ct.strokeStyle = '#00ff00';
+    this.ct.strokeRect(0, 0, this.cv.width, this.cv.height);
+  }
+
+  drawObj(x, y) {
+    this.ct.globalAlpha = 1.0;
+
+    this.ct.fillStyle = '#ccccff';
+    this.ct.fillRect(x, y, 24, 10);
+  }
+
+  drawPlayer(x, y) {
+    this.ct.globalAlpha = 1.0;
+
+    this.ct.beginPath();
+    this.ct.arc(x, y, 5, 0, Math.PI*2, true);
+    this.ct.fillStyle = '#ff8888';
+    this.ct.fill();
+  }
 }
