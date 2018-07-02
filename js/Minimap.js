c5c8160061b9af819261a82acc7f5cf84ff05052
bonochof <ryos427@yahoo.co.jp>
2018-07-02 15:50:34 +0900

    add alpha value

--- js/Minimap.js
+++ js/Minimap.js
@@ -5,6 +5,8 @@ function loadMap() {
 function drawMiniMap() {
   var cv = document.getElementById('miniMap');
   var ct = cv.getContext('2d');
+  
+  ct.globalAlpha = 0.8;
 
   ct.fillStyle = '#ccffcc';
   ct.fillRect(0, 0, cv.width, cv.height);
