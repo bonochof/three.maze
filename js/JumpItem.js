1a68660485f867b329c53085d79a2b5e9c5f7bea
bonochof <ryos427@yahoo.co.jp>
2018-07-03 15:44:55 +0900

    add move

--- js/JumpItem.js
+++ js/JumpItem.js
@@ -1,16 +1,18 @@
 class JumpItem extends GameObject {
   constructor() {
     super();
-    this.isItem = true;
+    this.objID = ITEM;
+    this.isGot = false;
     this.isVisible = true;
     this.mesh = new THREE.Mesh(
-      //new THREE.Sphere( new THREE.Vector3( 0, 0, 0 ), 2),
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshStandardMaterial({color: 0x338833, roughness: 0.0})
+      new THREE.SphereGeometry(2),
+      //new THREE.BoxGeometry(2, 2, 2),
+      new THREE.MeshStandardMaterial({color: 0x33ff33, roughness: 0.0})
     );
-    this.mesh.geometry.computeBoundingBox();  //BBAA作成
+    this.mesh.geometry.computeBoundingSphere();
     HitCheckTbl.push(this);
     scene.add(this.mesh);
+    this.dir = 0.1;
   }
 
   start(pos) {
@@ -19,14 +21,23 @@ class JumpItem extends GameObject {
     this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
   }
 
-  //ヒットした
+  update() {
+    this.pos.y += this.dir;
+    if (this.pos.y < 1 || this.pos.y > 4) {
+      this.dir = -this.dir;
+    }
+    this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
+  }
+
+  // get effect
   onHit(player, hitPoint) {
-    let dir = player.pos.clone().sub(hitPoint);  //ヒットした壁の点からプレイヤー方向のベクトルを生成
-    let k = player.sphere.radius - dir.length(); //壁にめり込んだ分の長さを生成(少し大きめに補正)
-    dir.normalize();
-    player.setPos(player.pos.clone().add(dir.clone().multiplyScalar(k)));
-    if (dir.dot(new THREE.Vector3(0, 1, 0)) > 0.99) {
-      player.y_speed = 0;
+    if (this.isGot) {
+      return;
+    } else {
+      this.mesh.visible = false;
+      this.isVisible = false;
+      player.jumpCount += 3;
+      this.isGot = true;
     }
   }
 }
